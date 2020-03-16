ff77f19a352966d60db250d51d995010e94e20af
jcarlosn <jcarlos.norte@gmail.com>
2020-03-16 21:02:22 +0100

    fixed build system and viewland

--- viewlang.sh
+++ viewlang.sh
@@ -7,7 +7,14 @@ rm -rf public
 [ -e source ] && unlink source
 
 ln -s source_$1 source
+
+
+cp _config.yml.template _config.yml
+sed -i '' 's/__LANG__/'$1'/g' _config.yml
+sed -i '' 's/__ROOT__//g' _config.yml
+
 npx hexo serve
 
 rm -rf public
 unlink source
+rm _config.yml
