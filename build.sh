76cbbcdf0fc4f836148b6eeff1fdd69e97df8fc1
jcarlosn <jcarlos.norte@gmail.com>
2020-03-16 22:37:47 +0100

    fixed build script

--- build.sh
+++ build.sh
@@ -5,33 +5,37 @@ npx hexo clean
 
 #clean public and source dirs in each lang build
 cleanBuild () {
-  rm -rf public
   unlink source
   npx hexo clean
 }
 
 #generates a language inside a subfolder and cleans before exit
 generateLang() {
-  ln -s source_en source
+  ln -s source_$1 source
   
   #change settings for this language
   cp _config.yml.template _config.yml
   sed -i '' 's/__LANG__/'$1'/g' _config.yml
 
 
-  npx hexo generate
-
   if [ $1 == "en" ]; then
-    cp -a public/. "../oxygen-project-public-site"
     sed -i '' 's/__ROOT__//g' _config.yml
     sed -i '' 's/__URL__/http\:\/\/oxygen\.protofy\.xyz/g' _config.yml
   else
-    cp -a public/. "../oxygen-project-public-site/$1"
     sed -i '' 's/__ROOT__/'$1'\//g' _config.yml
     sed -i '' 's/__URL__/http\:\/\/oxygen\.protofy\.xyz\/'$1'\//g' _config.yml
   fi
   
+  npx hexo generate
+
+  if [ $1 == "en" ]; then
+    cp -a public/. "../oxygen-project-public-site"
+  else
+    cp -a public/. "../oxygen-project-public-site/$1"
+  fi
+
   cleanBuild
+  rm -rf public
 }
 
 #should be clean, but still needed for idempotency
